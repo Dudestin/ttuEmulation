6160b4554519421ead51744afa051d0325515a42
Dudestin <splashakaponear@gmail.com>
2020-07-08 04:35:40 +0900

    mod. cmakefile

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dudestin/rayTrace/saarcor
+# Install script for directory: /home/dudestin/rayTrace/ttuEmulation
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/dudestin/rayTrace/saarcor/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dudestin/rayTrace/ttuEmulation/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main(void){
   Vector3f lower_left_corner = origin - horizontal/2 - vertical/2 - Vector3f(0,0,focal_length);
 
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
   for (int j = image_height-1; j>= 0;--j){
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i){
